45d989c275ed218bb54260fe890d3979737a5e1c
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2025-03-01 00:04:14 +0100

    feat: add new index page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,31 +5,20 @@ import "./globals.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
-import localFont from 'next/font/local'
-// import { Inter } from 'next/font/google'
  import { Suspense } from 'react'
-// // If loading a variable font, you don't need to specify the font weight
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Unlock My Data",
   description: "TODO",
 };
 
-const myFont = localFont({ src: './../fonts/Luciole/Luciole-Regular.ttf' })
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="fr"
-    style={{
-      fontFamily: myFont.style.fontFamily,
-    }}
-    >
-      <body className={`bg-white`}>
+    <html lang="fr">
+      <body>
         <Suspense fallback={<div>Loading...</div>}> <Nav /></Suspense>
-       
         <main
           role="main"
           id="contenu-principal"
