5d221defbe3a71fe5fea553588ef994fdc53cb6f
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2026-02-27 16:14:07 +0100

    ci: fix error and update system of release

---
+++ global.d.ts
@@ -0,0 +1,17 @@
+declare module '*.webp' {
+    import { StaticImageData } from 'next/image';
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module '*.png' {
+    import { StaticImageData } from 'next/image';
+    const content: StaticImageData;
+    export default content;
+}
+
+declare module '*.svg' {
+    import { StaticImageData } from 'next/image';
+    const content: StaticImageData;
+    export default content;
+}
